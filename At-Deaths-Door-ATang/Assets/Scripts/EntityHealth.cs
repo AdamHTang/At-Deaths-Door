6c8ec60d22146d7fb053a294523eaa05b804f1d7
Adam Tang <56270909+AdamHTang@users.noreply.github.com>
2021-12-04 16:23:18 -0600

    Player Death
    Player can no longer move when health is 0.

--- At-Deaths-Door-ATang/Assets/Scripts/EntityHealth.cs
+++ At-Deaths-Door-ATang/Assets/Scripts/EntityHealth.cs
@@ -5,6 +5,7 @@
  * Description: Health system for entities existing in game.
  */
 
+using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,13 +17,18 @@ public class EntityHealth : MonoBehaviour
     [SerializeField] private float _HealthPoints = 100f;
     private float maxHealth = 100f;
     public bool DestroyOnDeath = true;
+    private FirstPersonController fpController;
 
     public HealthBar healthBar;
 
-
+    void Awake()
+    {
+        fpController = gameObject.GetComponent<FirstPersonController>();
+    }
     void Start()
     {
         healthBar.SetMaxHealth(maxHealth);
+
     }
 
     public float HealthPoints
@@ -34,11 +40,18 @@ public class EntityHealth : MonoBehaviour
             if (HealthPoints <= 0)
             {
                 SendMessage("Die", SendMessageOptions.DontRequireReceiver);
+                fpController.MoveSpeed = 0.0f;
+                fpController.SpeedChangeRate = 0.0f;
+                fpController.JumpHeight = 0.0f;
+                fpController.RotationSpeed = 0.0f;
+                gameObject.transform.localRotation = Quaternion.Euler(-90.0f, transform.localRotation.y, transform.localRotation.z);
+                
 
                 if (DestroyOnDeath)
                 {
                     Destroy(gameObject);
                 }
+
             }
 
         } // End set()
